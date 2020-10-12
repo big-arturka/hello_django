0a541905a2ee3f0c62086259368d31a932c8e2cc
big-smoke <arturkrmnlv10@gmail.com>
2020-10-12 20:02:37 +0600

    Добавлено представление для получения Token-а.

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponseNotAllowed, HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-# Create your views here.
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
