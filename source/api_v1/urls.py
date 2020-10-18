bf9f65fd397b44349ff240764f25f0e7608e94d1
big-smoke <arturkrmnlv10@gmail.com>
2020-10-18 18:32:43 +0600

    Написаны представления для обновления и удаления статьи.

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -8,8 +8,8 @@ app_name = 'api_v1'
 urlpatterns = [
     path('get-token/', get_token_view, name='get_token'),
     path('articles/', ArticleListView.as_view(), name='article_list'),
-    path('article/create/', ArticleDeleteView.as_view(), name='article_detail'),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('article/create/', ArticleCreateView.as_view(), name='article_create'),
-    path('article/create/', ArticleUpdateView.as_view(), name='article_update'),
-    path('article/create/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('article/<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
+    path('article/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
 ]
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -3,6 +3,8 @@ import json
 from django.http import HttpResponseNotAllowed, HttpResponse, JsonResponse
 from django.views.generic import View
 from django.views.decorators.csrf import ensure_csrf_cookie
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from api_v1.serializers import ArticleSerializer
 from webapp.models import Article
@@ -15,49 +17,49 @@ def get_token_view(request, *args, **kwargs):
     return HttpResponseNotAllowed('Only GET request are allowed')
 
 
-class ArticleListView(View):
+class ArticleListView(APIView):
     def get(self, request, *args, **kwargs):
         objects = Article.objects.all()
         slr = ArticleSerializer(objects, many=True)
-        return JsonResponse(slr.data, safe=False)
+        return Response(slr.data)
 
 
-class ArticleCreateView(View):
+class ArticleDetailView(APIView):
+    def get(self, request, *args, **kwargs):
+        object = Article.objects.filter(pk=kwargs.get('pk'))
+        slr = ArticleSerializer(object, many=True)
+        return Response(slr.data)
+
+
+class ArticleCreateView(APIView):
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body)
         slr = ArticleSerializer(data=data)
         if slr.is_valid():
             article = slr.save()
-            return JsonResponse(slr.data, safe=False)
+            return Response(slr.data)
         else:
-            response = JsonResponse(slr.errors, safe=False)
+            response = Response(slr.errors)
             response.status_code = 400
             return response
 
 
-class ArticleDetailView(View):
-    def get(self, request, *args, **kwargs):
-        objects = Article.objects.filter(pk=kwargs.get('pk'))
-        slr = ArticleSerializer(objects)
-        return JsonResponse(slr.data, safe=False)
-
-
-class ArticleUpdateView(View):
+class ArticleUpdateView(APIView):
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body)
-        instance = Article.objects.filter(pk=kwargs.get('pk'))
+        instance = Article.objects.get(pk=kwargs.get('pk'))
         slr = ArticleSerializer(data=data, instance=instance)
         if slr.is_valid():
             article = slr.save()
-            return JsonResponse(slr.data, safe=False)
+            return Response(slr.data)
         else:
-            response = JsonResponse(slr.errors, safe=False)
+            response = Response(slr.errors)
             response.status_code = 400
             return response
         
 
-class ArticleDeleteView(View):
+class ArticleDeleteView(APIView):
     def post(self, request, *args, **kwargs):
-        objects = Article.objects.all()
-        slr = ArticleSerializer(objects, many=True)
-        return JsonResponse(slr.data, safe=False)
+        object = Article.objects.get(pk=kwargs.get('pk'))
+        object.delete()
+        return Response({'id': kwargs.get('pk')})
